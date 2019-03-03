05adc78fd631fe299e91468fb1e5163666fee3e7
vasylpb <vasylpb89@gmail.com>
2019-03-03 20:48:48 +0200

    express usage

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3000);
