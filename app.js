5c4ee3c3a53bbd0168d0cb93a3ac20d346b77a77
vasylpb <vasylpb89@gmail.com>
2019-03-07 22:12:47 +0200

    express

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
