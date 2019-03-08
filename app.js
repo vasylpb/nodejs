27afe47bb44cd2f9d43c432ea9191bec5a154c4e
vasylpb <vasylpb89@gmail.com>
2019-03-08 14:40:13 +0200

    ejs

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-	'hbs',
-	expressHbs({
-		layoutDir: 'views/layouts/',
-		defaultLayout: 'main-layout',
-		extname: 'hbs'
-	})
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
