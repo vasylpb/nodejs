ab7a7553184af5ea6ed99718dbc4424cf1bc8103
vasylpb <vasylpb89@gmail.com>
2019-03-08 13:54:37 +0200

    handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,10 @@ router.get('/add-product', (req, res, next) => {
 	// res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 	res.render('add-product', {
 		pageTitle: 'Add Product',
-		path: '/admin/add-product'
+		path: '/admin/add-product',
+		formsCss: true,
+		productCss: true,
+		activeAddProduct: true
 	});
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,14 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
 	const products = adminData.products;
 	// res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-	res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
+	res.render('shop', {
+		prods: products,
+		pageTitle: 'Shop',
+		path: '/',
+		hasProducts: products.length > 0,
+		activeShop: true,
+		productCss: true
+	});
 });
 
 module.exports = router;
