2206a0f9f2630c022bd26651f6a159a3384be371
vasylpb <vasylpb89@gmail.com>
2019-03-23 19:10:06 +0200

    dynamic routes

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,13 @@ router.get('/', shopContoller.getIndex);
 
 router.get('/products', shopContoller.getProducts);
 
+router.get('/products/:productId', shopContoller.getProduct);
+
 router.get('/cart', shopContoller.getCart);
+
+router.post('/cart', shopContoller.postCart);
+
+router.post('/cart-delete-item', shopContoller.postCartDeleteProduct);
 
 router.get('/orders', shopContoller.getOrders);
 
